b11a8a9862a9bcd2ecc66be002618e321e6a1394
vaibabu <97013298+vaibabu@users.noreply.github.com>
2022-01-17 13:47:07 +0530

    Implemented Pagination

--- src/app/components/course-card-list/course-card-list.component.ts
+++ src/app/components/course-card-list/course-card-list.component.ts
@@ -9,13 +9,34 @@ import { ICourse } from 'src/app/shared/models/course';
 })
 export class CourseCardListComponent implements OnInit {
   @Input() courses: Array<ICourse> = [];
-
+  public paginatedCourses: Array<Array<ICourse>> = [];
+  public pageSize: number = 4; // number of items displayed on each page
+  public currentPageIndex = 0;
   constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
-    this.courses = this.courses.splice(0, 5);
-    this.dataService.coursesInCart.subscribe((coursesInCart) =>
-      console.log(coursesInCart)
-    );
+    this.paginatedCourses = this.splitToPages();
+  }
+
+  public nextPage() {
+    this.currentPageIndex = this.currentPageIndex + 1;
+  }
+
+  public previousPage() {
+    this.currentPageIndex = this.currentPageIndex - 1;
+  }
+
+  public changePage(pageIndex: number) {
+    this.currentPageIndex = pageIndex;
+  }
+
+  private splitToPages() {
+    var results = [];
+
+    while (this.courses.length) {
+      results.push(this.courses.splice(0, this.pageSize));
+    }
+
+    return results;
   }
 }
