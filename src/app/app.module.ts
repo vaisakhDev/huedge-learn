53a6408847a2121688e188c007fb0e8bb4a6a271
vaibabu <97013298+vaibabu@users.noreply.github.com>
2022-01-18 14:18:06 +0530

    Implemented user profile details form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { CourseCardListModule } from './components/course-card-list/course-card-list.module';
 import { CartWidgetComponent } from './components/cart-widget/cart-widget.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ModalComponent } from './components/modal/modal.component';
 import { CartComponent } from './components/cart/cart.component';
 import { CourseDetailsComponent } from './components/course-details/course-details.component';
@@ -38,6 +38,7 @@ const routes = [
     BrowserModule,
     CourseCardListModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(routes)
   ],
   providers: [],
--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +7,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
+  public userForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) {
+    this.userForm = this.formBuilder.group({
+      displayName: ['', Validators.required],
+      firstName: ['', Validators.required],
+      lastName: [''],
+      aboutYourself: ['', Validators.maxLength(100)],
+      areaOfInterest: this.formBuilder.group({
+        designer: [false],
+        developer: [false],
+        projectManager: [true],
+        sales: [false]
+      }),
+      role: ['', Validators.maxLength(200)],
+      experience: ['0-5', Validators.required],
+      expertise: ['React', Validators.required],
+      userType: ['Student', Validators.required]
+    });
   }
 
+  ngOnInit(): void {}
+
+  get displayName() {
+    return this.userForm.get('displayName');
+  }
+
+  get firstName() {
+    return this.userForm.get('firstName');
+  }
+
+  get userType() {
+    return this.userForm.get('userType');
+  }
+
+  public onSave() {
+    if (this.userForm.valid) {
+      console.log(this.userForm.value);
+    } else {
+      return;
+    }
+  }
 }
